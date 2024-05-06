98ed0ffcc76874d17b07496d61fe70fe9b9e933f
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2024-05-06 13:17:55 -0400

    Create snake.java

---
+++ snake.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Snake;
+import com.zetcode.Board;
+//import Menu.Menu;
+//import java.awt.Component;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Snake extends JFrame {
+
+    public Snake() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+       
+        add(new Board());
+        //add(new Menu());
+        
+        
+        //board.setVisible(false);
+        //menu.setVisible(true);
+        
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
